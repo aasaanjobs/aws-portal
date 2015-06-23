420c030889f290f6429e2801412b2f937ace41b1
shubham <shubhamiitpowai@gmail.com>
2015-06-23 19:40:21 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from setuptools import setup
 
-setup(name='aws-portal-setup',
+setup(name='aws-portal',
   version='1.0',
   author='Shubham Bhartiya',
   author_email='bhartiya.shubham08@gmail.com',
   scripts=['aws-portal'],
-  install_requires=['boto', 'simplejson'],
+  url='https://github.com/aasaanjobs/aws-portal.git',
+  install_requires=['boto', 'simplejson']
 )
